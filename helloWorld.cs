b97db55981da02c5ec5e0da3e7245dffa5cf1085
Ducky229 <117349805+Ducky229@users.noreply.github.com>
2022-11-09 09:40:34 +0300

    Create helloWorld.cs

---
+++ helloWorld.cs
@@ -0,0 +1,11 @@
+using System;
+namespace helloWorld
+{
+    class Hello 
+    {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, world!");
+        }
+    }
+}
